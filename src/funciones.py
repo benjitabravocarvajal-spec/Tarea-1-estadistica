b34c88d2912adce0edf4cf28bbe7645b8cfc517b
Nyx <nyxbutelmann@gmail.com>
2026-09-25 15:54:22 -0300

    agregue varianza muestral

--- src/funciones.py
+++ src/funciones.py
@@ -1,4 +1,7 @@
 import numpy as np
+#definimos la función media muestral hasta el lanzamiento n-esimo, que recibe un array y un natural y devuelve la media muestral tomando los primeros n datos
 def media_muestral(n, x):
     return np.mean(x[:n])
-    
+#definimos la función varianza muestral hasta el lanzamiento n-esimo, que recibe un array y un natural y devuelve la varianza muestral tomando los primeros n datos
+def varianza_muestral(n, x):
+    return np.var(x[:n])
